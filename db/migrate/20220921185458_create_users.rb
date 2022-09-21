6f5dc92f79dffb6333dd502f3c8ab3d5d6a3b091
Yan Rivera <yan.rivera.6@gmail.com>
2022-09-21 12:23:29 -0700

    Add user model spec

--- db/migrate/20220921185458_create_users.rb
+++ db/migrate/20220921185458_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false 
-      t.string :password, null: false 
+      t.string :username, null: false, index: {unique: true}
+      t.string :password_digest, null: false 
+      t.string :session_token, null: false, index: {unique: true} 
       t.timestamps
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.create!(username: "mike", password: "password")}
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should have_many(:goals) }
+
+  describe "finds users by credentials" do 
+    context "with valid username and password" do 
+      it "returns correct user" do 
+        katie = User.create(username: "hansolo", password: "password")
+        user = User.find_by_credentials("hansolo", "password")
+
+        expect(katie.username).to eq(user.username)
+        expect(katie.password_digest).to eq(user.password_digest)
+      end
+    end
+    
+  end
 end
